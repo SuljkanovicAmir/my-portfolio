e0d9aa7ed9c83c4831198b4c6ba47ec00cd5a686
Amir <suljkanovic.amir@hotmail.com>
2022-03-26 21:22:42 +0100

    Second responsive commit with new design

--- script.js
+++ script.js
@@ -29,85 +29,6 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
-/*
-let i = 0, myTitle;
-myTitle = "Hello, I'm Amir Suljkanović.\r\nI'm a front-end web developer.";
-
-function typing () {
-  if(i < myTitle.length) {
-    document.querySelector('#mytitle').textContent += myTitle.charAt(i);
-    i++;
-    setTimeout(typing, 50);
-  }
-}
-setTimeout(typing, 1000);
-
-let j = 0, myP;
-myP = "I have 1 year experience at HTML, CSS and Javascript. \r\nI value honesty and integrity above all else. These ideals \r\nguide my approach to problem solving and life in general." ;
-/*
-/*
-function typingP () {
-  if(j < myP.length) {
-    document.querySelector('#myp').textContent += myP.charAt(j);
-    j++;
-    setTimeout(typingP, 30);
-  }
-}
-
-
-setTimeout(typingP, 3500);
-*/
-function validate() {
-let name = document.querySelector("#name");
-let email = document.querySelector("#email");
-let msg = document.querySelector("#message");
-let btn = document.querySelector("#btn");
-
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (name.value == "" || email.value == "" || msg.value == "") {
-    emptyerror();
-  } else {
-    sendMail(name.value, email.value, msg.value);
-    success();
-  }
-});
-}
-validate();
- 
-
-function sendMail (name, email, msg) {
-  emailjs.send('portfolio_email', 'template_nrdx09h',{
-  email: email,
-  name: name,
-  msg: msg,
-  });
-};
-
-function emptyerror() {
-  Swal.fire({
-    title: "Fields can't be empty!",
-  });
-}
-
-function error() {
-  Swal.fire({
-    icon: "error",
-    title: "Something went wrong!",
-  });
-}
-
-function success() {
-  Swal.fire({
-    icon: "success",
-    title: "Successfully sent message",
-  });
-}
- 
-function swalMessage () {
-  Swal.fire('Links to projects will be added later');
-}
-
 
 
   document.querySelectorAll('.fade-in'); 
@@ -152,6 +73,10 @@ function showPage() {
   points: 15.00,
   showDots: false
   })
+  AOS.init({
+    duration: 1000,
+    delay: 100,
+  });
 }
 
 
@@ -165,6 +90,8 @@ function darkMode() {
   document.querySelector('.mytitle').classList.toggle('dark')
   document.querySelector('.main-container').classList.toggle('dark')
   document.querySelector('.aboutMe').classList.toggle('dark')
+  document.querySelector('.form-container').classList.toggle('dark')
+  document.querySelector('.form-text').classList.toggle('dark')
   document.querySelectorAll('.text').forEach(function(field) {
     field.classList.toggle('dark')
     });
@@ -214,5 +141,6 @@ if (mainBody.classList.contains('dark')) {
     showDots: false
     })
   }
+ 
 } 
 
